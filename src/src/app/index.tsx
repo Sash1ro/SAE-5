c190d520ded40ba7b6e1cc937adba129feb6e34d
Mattéo Plancher <matteoplancher@Ordinateur-portable-de-Matteo.local>
2026-09-17 09:39:48 +0200

    reset to build the app from scratch

--- src/src/app/index.tsx
+++ src/src/app/index.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
@@ -11,8 +11,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
